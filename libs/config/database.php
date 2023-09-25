e324e4f795e823e40ca023972a6cb51f0e5a3b4c
duonghuupha <duonghuupha@hotmail.com.vn>
2023-09-25 15:35:52 +0700

    asdf

--- libs/config/database.php
+++ libs/config/database.php
@@ -1,15 +1,7 @@
 <?php
-if(!isset($_SESSION['lang'])){
-    define('DB_TYPE', 'mysql');
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'svkherbal_vi');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-}else{
-    define('DB_TYPE', 'mysql');
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'svkherbal_en');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-}
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ivyvet');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 ?>
